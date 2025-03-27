b5a5accb31fa3c856d5736f70d9ada159da67d8a
Filippo Biondi <filibiondi2000@gmail.com>
2025-03-27 23:13:09 +0100

    Added first configuration for macbook.

---
+++ hosts/macbook-pro/default.nix
@@ -0,0 +1,105 @@
+{
+  outputs,
+  userConfig,
+  pkgs,
+  ...
+}: {
+ # Add nix-homebrew configuration
+  nix-homebrew = {
+    enable = true;
+    enableRosetta = true;
+    user = "${userConfig.name}";
+    autoMigrate = true;
+  };
+
+  # Nixpkgs configuration
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.nvim
+    ];
+
+    config.allowUnfree = true;
+  };
+
+  # Nix settings
+  nix = {
+    settings = {
+      experimental-features = "nix-command flakes";
+    };
+    optimise.automatic = true;
+    package = pkgs.nix;
+  };
+
+  # User configuration
+  users.users.${userConfig.name} = {
+    name = "${userConfig.name}";
+    home = "/Users/${userConfig.name}";
+  };
+
+  # Add ability to use TouchID for sudo
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  # System settings
+  system = {
+    defaults = {
+      ".GlobalPreferences" = {
+        "com.apple.mouse.tapBehavior" = 1;
+      };
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        AppleShowAllExtensions = true;
+        NSDocumentSaveNewDocumentsToCloud = false;
+        NSNavPanelExpandedStateForSaveMode = true;
+        PMPrintingExpandedStateForPrint = true;
+      };
+      LaunchServices = {
+        LSQuarantine = false;
+      };
+      trackpad = {
+        TrackpadRightClick = true;
+        TrackpadThreeFingerDrag = true;
+        Clicking = true;
+      };
+      finder = {
+        AppleShowAllFiles = true;
+        CreateDesktop = false;
+        FXDefaultSearchScope = "SCcf";
+        FXEnableExtensionChangeWarning = false;
+        FXPreferredViewStyle = "Nlsv";
+        QuitMenuItem = true;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+        _FXShowPosixPathInTitle = true;
+        _FXSortFoldersFirst = true;
+      };
+      dock = {
+        autohide = true;
+        show-recents = false;
+        showhidden = true;
+        persistent-apps = [];
+      };
+      screencapture = {
+        location = "/Users/${userConfig.name}/Downloads/temp";
+        type = "png";
+        disable-shadow = true;
+      };
+    };
+  };
+
+  # Zsh configuration
+  programs.zsh.enable = true;
+
+  # Fonts configuration
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    roboto
+  ];
+
+  homebrew = {
+    enable = true;
+    enableRosetta = true;
+  };
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  system.stateVersion = 6;
+}
