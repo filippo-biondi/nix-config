fb0b9d3105ff628e4c3c511acc0c0819282931ec
Filippo Biondi <filibiondi2000@gmail.com>
2025-03-29 09:04:52 +0100

    Moved home-manager config as darwin module and modularized darwin config.

---
+++ modules/darwin/common/default.nix
@@ -0,0 +1,65 @@
+{
+  outputs,
+  pkgs,
+  userConfig,
+  ...
+}:{
+ # Add nix-homebrew configuration
+  nix-homebrew = {
+    enable = true;
+    enableRosetta = true;
+    user = "${userConfig.name}";
+  };
+
+  homebrew = {
+    enable = true;
+    brews = [];
+    casks = [];
+    taps = [];
+    masApps = {};
+    global.autoUpdate = false;
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "zap";
+      upgrade = true;
+    };
+  };
+
+  # Nixpkgs configuration
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.nvim
+    ];
+
+    hostPlatform = "aarch64-darwin";
+
+    config.allowUnfree = true;
+  };
+
+  # Nix settings
+  nix = {
+    settings = {
+      experimental-features = "nix-command flakes";
+    };
+    optimise.automatic = true;
+    package = pkgs.nix;
+  };
+
+  # User configuration
+  users.users.${userConfig.name} = {
+    name = "${userConfig.name}";
+    home = "/Users/${userConfig.name}";
+  };
+
+  # Add ability to use TouchID for sudo
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  # Zsh configuration
+  programs.zsh.enable = true;
+
+  # Fonts configuration
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    roboto
+  ];
+}
---
+++ modules/darwin/settings/default.nix
@@ -0,0 +1,47 @@
+{
+  userConfig,
+  ...
+}: {
+  # System settings
+  system = {
+    defaults = {
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        AppleShowAllExtensions = true;
+        NSDocumentSaveNewDocumentsToCloud = false;
+        NSNavPanelExpandedStateForSaveMode = true;
+        PMPrintingExpandedStateForPrint = true;
+      };
+      LaunchServices = {
+        LSQuarantine = false;
+      };
+      trackpad = {
+        Dragging = true;
+        Clicking = true;
+      };
+      finder = {
+        AppleShowAllFiles = true;
+        CreateDesktop = false;
+        FXDefaultSearchScope = "SCcf";
+        FXEnableExtensionChangeWarning = false;
+        FXPreferredViewStyle = "Nlsv";
+        QuitMenuItem = true;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+        _FXShowPosixPathInTitle = true;
+        _FXSortFoldersFirst = true;
+      };
+      dock = {
+        autohide = true;
+        show-recents = false;
+        showhidden = true;
+        persistent-apps = [];
+      };
+      screencapture = {
+        location = "/Users/${userConfig.name}/Downloads/temp";
+        type = "png";
+        disable-shadow = true;
+      };
+    };
+  };
+}
