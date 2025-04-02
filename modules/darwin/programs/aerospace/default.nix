b7eab1573072b3a62c640433555a9852ae0c01e6
Filippo Biondi <filibiondi2000@gmail.com>
2025-04-02 17:48:10 +0200

    Added shortcut for move-node-to-workspace and then move-to-workspace.

--- modules/darwin/programs/aerospace/default.nix
+++ modules/darwin/programs/aerospace/default.nix
@@ -79,6 +79,38 @@
         alt-shift-y = "move-node-to-workspace Y";
         alt-shift-z = "move-node-to-workspace Z";
 
+        alt-cmd-shift-1 = ["move-node-to-workspace 1" "workspace 1"];
+        alt-cmd-shift-2 = ["move-node-to-workspace 2" "workspace 2"];
+        alt-cmd-shift-3 = ["move-node-to-workspace 3" "workspace 3"];
+        alt-cmd-shift-4 = ["move-node-to-workspace 4" "workspace 4"];
+        alt-cmd-shift-5 = ["move-node-to-workspace 5" "workspace 5"];
+        alt-cmd-shift-6 = ["move-node-to-workspace 6" "workspace 6"];
+        alt-cmd-shift-7 = ["move-node-to-workspace 7" "workspace 7"];
+        alt-cmd-shift-8 = ["move-node-to-workspace 8" "workspace 8"];
+        alt-cmd-shift-9 = ["move-node-to-workspace 9" "workspace 9"];
+        alt-cmd-shift-a = ["move-node-to-workspace A" "workspace A"];
+        alt-cmd-shift-b = ["move-node-to-workspace B" "workspace B"];
+        alt-cmd-shift-c = ["move-node-to-workspace C" "workspace C"];
+        alt-cmd-shift-d = ["move-node-to-workspace D" "workspace D"];
+        alt-cmd-shift-e = ["move-node-to-workspace E" "workspace E"];
+        alt-cmd-shift-f = ["move-node-to-workspace F" "workspace F"];
+        alt-cmd-shift-g = ["move-node-to-workspace G" "workspace G"];
+        alt-cmd-shift-i = ["move-node-to-workspace I" "workspace I"];
+        alt-cmd-shift-m = ["move-node-to-workspace M" "workspace M"];
+        alt-cmd-shift-n = ["move-node-to-workspace N" "workspace N"];
+        alt-cmd-shift-o = ["move-node-to-workspace O" "workspace O"];
+        alt-cmd-shift-p = ["move-node-to-workspace P" "workspace P"];
+        alt-cmd-shift-q = ["move-node-to-workspace Q" "workspace Q"];
+        alt-cmd-shift-r = ["move-node-to-workspace R" "workspace R"];
+        alt-cmd-shift-s = ["move-node-to-workspace S" "workspace S"];
+        alt-cmd-shift-t = ["move-node-to-workspace T" "workspace T"];
+        alt-cmd-shift-u = ["move-node-to-workspace U" "workspace U"];
+        alt-cmd-shift-v = ["move-node-to-workspace V" "workspace V"];
+        alt-cmd-shift-w = ["move-node-to-workspace W" "workspace W"];
+        alt-cmd-shift-x = ["move-node-to-workspace X" "workspace X"];
+        alt-cmd-shift-y = ["move-node-to-workspace Y" "workspace Y"];
+        alt-cmd-shift-z = ["move-node-to-workspace Z" "workspace Z"];
+
         alt-tab = "workspace-back-and-forth";
         alt-shift-tab = "move-workspace-to-monitor --wrap-around next";
 
