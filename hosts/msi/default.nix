5b1257bda98193558b208dd262f875a50605d3af
Filippo Biondi <filibiondi2000@gmail.com>
2025-04-13 19:07:53 +0200

    Added separated conifg for BIOS and UEFI hosts

--- hosts/msi/default.nix
+++ hosts/msi/default.nix
@@ -4,6 +4,7 @@
 }: {
   imports = [
     ./hardware-configuration.nix
+    "${nixosModules}/features/boot/uefi"
     "${nixosModules}/common"
     "${nixosModules}/graphics"
     "${nixosModules}/nvidia"
---
+++ modules/nixos/features/boot/uefi/default.nix
@@ -0,0 +1,10 @@
+{ ... }: {
+  # Bootloader.
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
+    efi.canTouchEfiVariables = true;
+  };
+}
