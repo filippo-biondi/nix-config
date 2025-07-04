dd588fb2700fa9c45a327d88537ab25d912f9607
Filippo Biondi <filibiondi2000@gmail.com>
2025-07-04 15:46:32 +0200

    Added substituters also for nixos

--- config/nixos/common/default.nix
+++ config/nixos/common/default.nix
@@ -28,6 +28,12 @@
     settings = {
       experimental-features = ["nix-command" "flakes"];
       auto-optimise-store = true;
+      substituters = [
+        "https://devenv.cachix.org"
+      ];
+      trusted-public-keys = [
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      ];
     };
     gc = {
       automatic = true;
