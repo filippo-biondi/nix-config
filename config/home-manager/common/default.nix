3dd31e816d6f0bdef07b25c410bfb67a60b982e1
Filippo Biondi <filibiondi2000@gmail.com>
2025-07-10 10:22:31 +0200

    Added devour-flake

--- config/home-manager/common/default.nix
+++ config/home-manager/common/default.nix
@@ -12,6 +12,7 @@
   nixpkgs = {
     overlays = [
       outputs.overlays.nvim
+      outputs.overlays.devour-flake
       outputs.overlays.devenv
     ] ++ (if isDarwin then [ outputs.overlays.qt_fix ] else []);
 
--- overlays/default.nix
+++ overlays/default.nix
@@ -4,24 +4,28 @@
 }: {
   # When applied, the stable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.stable'
-  stable-packages = final: _prev: {
+  stable-packages = final: prev: {
     stable = import inputs.nixpkgs-stable {
       system = final.system;
       config.allowUnfree = true;
     };
   };
 
-  old-packages = final: _prev: {
+  old-packages = final: prev: {
     old = import inputs.nixpkgs-old {
       system = final.system;
       config.allowUnfree = true;
     };
   };
-  nightly-packages = final: _prev: {
+  nightly-packages = final: prev: {
     nightly = import inputs.nixpkgs-nightly {
       system = final.system;
       config.allowUnfree = true;
     };
+  };
+
+  devour-flake = final: prev: {
+    devour-flake = final.callPackage inputs.devour-flake { };
   };
 
   nvim = inputs.nvim.overlays.default;
