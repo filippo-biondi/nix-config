1be44cac8f3c203c289acc71118512966a243159
Filippo Biondi <filibiondi2000@gmail.com>
2025-08-02 18:38:48 +0200

    Added UTM to darwin features

--- features/desktop-apps/misc/darwin/default.nix
+++ features/desktop-apps/misc/darwin/default.nix
@@ -1,7 +1,14 @@
-{ ... }: {
+{
+  pkgs,
+  ...
+}: {
   homebrew.casks = [
     "balenaetcher"
     "ultimaker-cura"
     "kicad"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    utm
   ];
 }
