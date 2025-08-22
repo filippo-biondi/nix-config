79647d5b568ba21c70b59fb414dbee981aab787a
Filippo Biondi <filibiondi2000@gmail.com>
2025-08-22 14:20:27 +0200

    Added root login on raspberry for remote build

--- hosts/server-casa/default.nix
+++ hosts/server-casa/default.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   configFolder,
   featuresFolder,
   suffix_imports,
@@ -14,7 +15,15 @@
     "${configFolder}/wifi"
     "${featuresFolder}/core/docker"
     "${featuresFolder}/network/tailscale"
+    "${featuresFolder}/network/blocky"
   ];
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIArV7DUbiqZYLwtF5tZVQTskVPYJzaltXqZzVYJrxJwy"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINi5XH2x57j86zBf2eMDkEhjHBeIOuGdxWsc358WfcQT"
+  ];
+
+  services.openssh.permitRootLogin = lib.mkForce "prohibit-password";
 
   system.stateVersion = "24.05"; # Did you read the comment?
 }
