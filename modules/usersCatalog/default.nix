ad92d6b7b1aeb46c95e9722cbce05f01a1eb055c
Filippo Biondi <filibiondi2000@gmail.com>
2026-02-03 23:52:56 +0100

    Add missing data in usersCatalog

--- modules/usersCatalog/default.nix
+++ modules/usersCatalog/default.nix
@@ -16,16 +16,20 @@
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE/YrxCxrCU2AC8CPuDSgwovd4KHoacdoDfuveiX5FVG"
         ];
         shell = pkgs.zsh;
+        extraGroups = [ "wheel" "networkmanager" "docker" ];
+        setPassword = true;
       };
       matteo = {
         fullName = "Matteo Tolloso";
         sshKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICYhWjAETWEB1YdT3Hn1xDEiJWbtAScaoi5+auEq1SQM" ];
         shell = pkgs.bash;
+        extraGroups = [ "docker" ];
       };
       vornao = {
         fullName = "Luca Miglior";
         sshKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF65PNkzAPb12J2BV/Jzc79+BZ8RIlLJPDz6tOta21Cj" ];
         shell = pkgs.bash;
+        extraGroups = [ "docker" ];
       };
     };
 
